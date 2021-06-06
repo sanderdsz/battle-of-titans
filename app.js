44378c85ac5ff9525731239915affe12a7507892
sanderdsz <sanderdsz@gmail.com>
2021-06-06 09:41:59 -0300

    special attack function

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
-      monsterHealth: 100
+      monsterHealth: 100,
+      currentRound: 0,
     }
   },
   computed: {
@@ -15,10 +16,14 @@ const app = Vue.createApp({
     },
     playerBarStyles() {
       return { width: this.playerHealth + '%' };
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
     }
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       // Attacks need to be a random number at least 5 points to maximum 12 points
       const attackValue = getRandomValue(12, 5);
       this.monsterHealth -= attackValue;
@@ -29,6 +34,12 @@ const app = Vue.createApp({
       const attackValue = getRandomValue(15, 8);
       this.playerHealth -= attackValue;
     },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    }
   }
 })
 
